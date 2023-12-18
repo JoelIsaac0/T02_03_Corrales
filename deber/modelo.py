67ff7e8837f2b3c54f1e04c258155dc15006eda0
JoelIsaac0 <118613482+JoelIsaac0@users.noreply.github.com>
2023-12-18 13:46:49 -0500

    clase usuario

---
+++ deber/modelo.py
@@ -0,0 +1,9 @@
+class Usuario:
+    def __init__(self, id, nombre, apellido, correo, cedula, celular):
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellido
+        self.correo = correo
+        self.cedula = cedula
+        self.celular = celular
+
