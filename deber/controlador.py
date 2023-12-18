cbd8f73f42e7b69d98303260e69f7b554e6870dc
JoelIsaac0 <118613482+JoelIsaac0@users.noreply.github.com>
2023-12-18 16:49:19 -0500

    swagger_FASTAPI

--- deber/controlador.py
+++ deber/controlador.py
@@ -33,15 +33,57 @@ class Controlador:
         numero_cuenta = input("Ingrese el número de cuenta a eliminar: ")
 
         self.servicio.eliminar_cuenta_y_usuario(numero_cuenta)
-	
+
+    def solicitar_credito_desde_consola(self):
+        numero_cuenta = input("Ingrese el número de cuenta del usuario: ")
+        monto = float(input("Ingrese el monto del crédito: "))
+        plazo_meses = int(input("Ingrese el plazo del crédito en meses: "))
+        tasa_interes = float(input("Ingrese la tasa de interés del crédito (porcentaje): "))
+
+        credito_id = self.servicio.solicitar_credito(numero_cuenta, monto, plazo_meses, tasa_interes)
+        print(f"Crédito solicitado con ID: {credito_id}")
+
+    def aprobar_credito_desde_consola(self):
+        numero_cuenta = input("Ingrese el número de cuenta del crédito a aprobar: ")
+        self.servicio.aprobar_credito(numero_cuenta)
+        print(f"Crédito para la cuenta {numero_cuenta} aprobado")
+
+    def rechazar_credito_desde_consola(self):
+        numero_cuenta = input("Ingrese el número de cuenta del crédito a rechazar: ")
+        self.servicio.rechazar_credito(numero_cuenta)
+        print(f"Crédito para la cuenta {numero_cuenta} rechazado")
+
+    def generar_tabla_amortizacion_desde_consola(self):
+        numero_cuenta = input("Ingrese el número de cuenta del crédito para generar la tabla de amortización: ")
+        self.servicio.generar_tabla_amortizacion(numero_cuenta)
+    def generar_reporte_creditos_desde_consola(self):
+        numero_cuenta = input("Ingrese el número de cuenta del usuario para generar el reporte: ")
+        self.servicio.generar_reporte_creditos(numero_cuenta)
+
+
+    def obtener_usuarios_desde_consola(self):
+        usuarios = self.repositorio.obtener_usuarios()
+        if usuarios:
+            print("Lista de Usuarios:")
+            for usuario in usuarios:
+                print(usuario)
+        else:
+            print("No hay usuarios registrados.")
+
+
+
+
 if __name__ == "__main__":
     controlador = Controlador()
     #DESCOMENTAR PARA REALIZAR LAS RESPECTIVAS FUNCIONES
     
-    controlador.registrar_usuario_desde_consola()
+    #controlador.registrar_usuario_desde_consola()
     #controlador.realizar_deposito_desde_consola()
     #controlador.eliminar_cuenta_y_usuario_desde_consola()
-    controlador.realizar_deposito_desde_consola()
-    controlador.realizar_retiro_desde_consola()
+    #controlador.realizar_deposito_desde_consola()
+    #controlador.realizar_retiro_desde_consola()
+    controlador.solicitar_credito_desde_consola()
+    controlador.generar_reporte_creditos_desde_consola()
+
     # Cerrar la conexión al finalizar
     controlador.repositorio.cerrar_conexion()
